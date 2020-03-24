eedd32cb62a257fe55ffee024de099b0a43b4a94
张牡丹 <1050339760@qq.com>
2020-03-24 16:29:49 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    //除了首页之外的都可以懒加载
+    component: () => import('@/views/Login')
   }
 ]
 
